c8aa6e9f1a5e75680eff0f6538831c6132eb18f3
Kacey Cheng <kaceyc@Kaceys-Mac-mini.local>
2026-04-14 19:25:01 -0700

    fix: extreme cache busting with random UUID to force 60 cards update

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,3 +1,4 @@
+// FORCE_UPDATE_KEY = "7214affe-c79d-4e23-921e-0d93a53a227e"
 import { Suspense } from "react";
 import { getAllCards, getAllIssuers, getAllTags } from "@/lib/cards";
 import CardGrid from "@/app/components/CardGrid";
